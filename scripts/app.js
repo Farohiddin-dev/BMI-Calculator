ef7038001d4bcb12f1c629f04cb28a14429868ae
Kamoldinov Farohiddin <67187364+Farohiddin-dev@users.noreply.github.com>
2021-08-15 12:16:33 +0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -15,31 +15,43 @@ let javobi = document.getElementById("javobi");
 let hisobla = document.getElementById("hisobla");
 let natija = null;
 
+let reklama = document.getElementById("reklama");
+let reklama2 = document.getElementById("reklama2");
+
 hisobla.onclick = function(){
    natija = (mass.value / ((height.value * height.value) / 10000)).toFixed(2);
    if (natija<=16) {
-    javobi.innerHTML = "Siz juda ozg'insiz! (yuqori ingichka)<strong>😔😔</strong>";
+    javobi.innerHTML = "Siz juda ozg'insiz! (yuqori ingichka)<strong>😔😔, <br>Semirishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama2.style.display = "flex";
    }
    else if (natija<=16.9 && natija>=16) {
-    javobi.innerHTML = "Siz ozg'insiz! (o'rtacha)<strong>😔</strong>";
+    javobi.innerHTML = "Siz ozg'insiz! (o'rtacha)<strong>😔<br>Semirishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama2.style.display = "flex";
    }
    else if (natija<=18.4 && natija>=17) {
-    javobi.innerHTML = "Siz juda ozg'insiz. (boshlang'ich)<strong>😞</strong>";
+    javobi.innerHTML = "Siz juda ozg'insiz. (boshlang'ich)<strong>😞<br>Semirishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama2.style.display = "flex";
    }
    else if (natija<=24.9 && natija>=18.5) {
     javobi.innerHTML = "Sizning vazninggiz yaxshi, ovqat rastioninga to'g'ri amal qlishda davom eting!<strong>☺️☺️</strong>";
+    reklama.style.display = "none";
+    reklama2.style.display = "none";
    }
    else if ( natija<= 29.9 && natija>=25 ) {
     javobi.innerHTML = "Sizning vazninggiz ortiqcha, semirish arafasida turbsiz!<strong>😞😞</strong>";
+    reklama.style.display = "flex";
    }
    else if ( natija<=34.9 && natija>=30 ) {
-    javobi.innerHTML = "Siz semizlikning I sinfidasiz!<strong>😞😔</strong>";
+    javobi.innerHTML = "Siz semizlikning I sinfidasiz!<strong>😞😔<br>Ozishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama.style.display = "flex";
    }
    else if ( natija<=39.9 && natija>=35 ) {
-    javobi.innerHTML = "Siz semizlikning II sinfidasiz!<strong>😔</strong>";
+    javobi.innerHTML = "Siz semizlikning II sinfidasiz!<strong>😔<br>Ozishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama.style.display = "flex";
    }
    else if ( natija>=40) {
-    javobi.innerHTML = "Siz semizlikning III sinfidasiz! <strong>😔😔</strong>";
+    javobi.innerHTML = "Siz semizlikning III sinfidasiz! <strong>😔😔<br>Ozishingiz uchun, sizga ushbu mahsulotlarni tavsiya qilamiz:</strong>";
+    reklama.style.display = "flex";
    }
    console.log(natija)
    
